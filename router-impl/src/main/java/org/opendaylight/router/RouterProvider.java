5cef5e33a9241fce5487358266c0c828349dae87
subhash kumar singh <subh.singh007@gmail.com>
2016-03-31 02:05:43 +0530

    Implementation to receive the packet-in message.

--- router-impl/src/main/java/org/opendaylight/router/RouterProvider.java
+++ router-impl/src/main/java/org/opendaylight/router/RouterProvider.java
@@ -10,12 +10,22 @@ package org.opendaylight.router;
 
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceived;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.NotificationListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class RouterProvider implements BindingAwareProvider, AutoCloseable {
+public class RouterProvider implements BindingAwareProvider, AutoCloseable, PacketProcessingListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(RouterProvider.class);
+    private ListenerRegistration<NotificationListener> listener;
+
+    public RouterProvider(NotificationProviderService notificationProviderService) {
+        listener = notificationProviderService.registerNotificationListener(this);
+    }
 
     @Override
     public void onSessionInitiated(ProviderContext session) {
@@ -24,6 +34,12 @@ public class RouterProvider implements BindingAwareProvider, AutoCloseable {
 
     @Override
     public void close() throws Exception {
+        this.listener.close();
+    }
+
+    @Override
+    public void onPacketReceived(PacketReceived packet) {
+        LOG.debug("reveived the packet :" + packet.toString());
     }
 
 }
--- router-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/router/impl/rev141210/RouterImplModule.java
+++ router-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/router/impl/rev141210/RouterImplModule.java
@@ -1,4 +1,7 @@
 package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.router.impl.rev141210;
+
+import org.opendaylight.router.RouterProvider;
+
 public class RouterImplModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.router.impl.rev141210.AbstractRouterImplModule {
     public RouterImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
@@ -15,8 +18,10 @@ public class RouterImplModule extends org.opendaylight.yang.gen.v1.urn.opendayli
 
     @Override
     public java.lang.AutoCloseable createInstance() {
-        // TODO:implement
-        throw new java.lang.UnsupportedOperationException();
+
+        RouterProvider provider = new RouterProvider(getNotificationServiceDependency());
+        getBrokerDependency().registerProvider(provider);
+        return provider;
     }
 
 }
