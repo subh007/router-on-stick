ddffa57a7b6d02b791b4174d653e1ac87ccc9b24
subhash kumar singh <subh.singh007@gmail.com>
2016-04-07 01:34:12 +0530

    Add utility method for serialization of header

---
+++ router-impl/src/main/java/org/opendaylight/packet/PacketSerializer.java
@@ -0,0 +1,44 @@
+package org.opendaylight.packet;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.arp.packet.received.packet.chain.packet.ArpPacket;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ethernet.rev140528.Header8021q;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.ethernet.rev140528.ethernet.packet.received.packet.chain.packet.EthernetPacket;
+
+public class PacketSerializer {
+
+    /**
+     * This method serializes the ethernet header.
+     * @param etherHeader
+     * @return
+     */
+    public static byte[] serializeEthernetHeader(EthernetPacket etherHeader) {
+        return null;
+    }
+
+    /**
+     * This method serializes the ARP header.
+     * @param arpHeader
+     * @return
+     */
+    public static byte[] serializeArpHeader(ArpPacket arpHeader) {
+        return null;
+    }
+
+    /**
+     * This method serializes the vlan header.
+     * @param vlanHeader
+     * @return
+     */
+    public static byte[] serializeVlanHeader(Header8021q vlanHeader) {
+        return null;
+    }
+    /**
+     * This method combines the all the headers and returns as single
+     * byte stream.
+     * @param headers
+     * @return
+     */
+    public static byte[] joinHeader(byte[]... headers) {
+        return null;
+    }
+}
