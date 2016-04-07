d9e129edf8f52f9ecd7265b1bd25e84e9a54d407
subhash kumar singh <subh.singh007@gmail.com>
2016-04-07 16:49:40 +0530

    Add license header to packet parser

--- router-impl/src/main/java/org/opendaylight/packet/PacketSerializer.java
+++ router-impl/src/main/java/org/opendaylight/packet/PacketSerializer.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2015 Yoyodyne, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.packet;
 
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.arp.rev140528.arp.packet.received.packet.chain.packet.ArpPacket;
