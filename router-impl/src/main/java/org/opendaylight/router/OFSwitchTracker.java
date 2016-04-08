07f3b051c04a2af6c6e8cc55de7507ea39e49628
subhash kumar singh <subh.singh007@gmail.com>
2016-04-09 02:08:14 +0530

    Implementation of default flow rule

--- router-impl/src/main/java/org/opendaylight/router/OFSwitchTracker.java
+++ router-impl/src/main/java/org/opendaylight/router/OFSwitchTracker.java
@@ -7,11 +7,22 @@
  */
 package org.opendaylight.router;
 
+import java.util.Arrays;
 import java.util.Map;
 
 import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Uri;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.OutputActionCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.action.output.action._case.OutputActionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.ActionBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.OutputPortValues;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.InstructionsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.WriteActionsCaseBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.instruction.write.actions._case.WriteActionsBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.instruction.list.InstructionBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
@@ -39,6 +50,39 @@ public class OFSwitchTracker implements DataChangeListener {
 
     public void installDefaultFlowRule(Node node) {
 
+        // Create Action to forward the packet to controller
+        ActionBuilder actBuilder = new ActionBuilder();
+        actBuilder.setAction(
+                new OutputActionCaseBuilder()
+                .setOutputAction(
+                        new OutputActionBuilder()
+                        .setOutputNodeConnector(
+                                new Uri(OutputPortValues.CONTROLLER.toString()))
+                        .build())
+                .build())
+        .setOrder(0);
+
+        // Create instruction
+        InstructionBuilder instBuilder = new InstructionBuilder();
+        instBuilder.setInstruction(
+                new WriteActionsCaseBuilder()
+                .setWriteActions(
+                        new WriteActionsBuilder()
+                        .setAction(Arrays.asList(actBuilder.build()))
+                        .build())
+                .build())
+        .setOrder(0);
+
+        // create flow
+        FlowBuilder flowBuilder = new FlowBuilder();
+        flowBuilder.setBufferId(new Long(-1))
+        .setInstructions(
+                new InstructionsBuilder()
+                .setInstruction(
+                        Arrays.asList(instBuilder.build()))
+                .build()
+                )
+        .setTableId((short)0);
     }
 
 }
