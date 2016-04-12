aaf48eca55fa129414b51f56ecf3ee99b7a51f13
subhash kumar singh <subh.singh007@gmail.com>
2016-04-13 00:41:19 +0530

    Update topology file to reflect the test topology

--- mininet-scripts/test-topology.sh
+++ mininet-scripts/test-topology.sh
@@ -3,31 +3,45 @@
 # blue ------ br0 (ovs) --------- red
 # 1.0.0.1--(vlan 100)---- ovs ----(vlan 200)------ 1.0.0.2
 # add namespaces
-sudo ip netns add red
-sduo ip netns add blue
+ip netns add red
+ip netns add blue
 
 # create the veth pairs
 ip link add veth0 type veth peer name veth1
 ip link add veth2 type veth peer name veth3
+ip link add veth4 type veth peer name veth5 # for br1 <----> br0
+ip link add veth6 type veth peer name veth7 # for br2 <----> br0
 
 ifconfig veth0 up
 ifconfig veth1 up
 ifconfig veth2 up
 ifconfig veth3 up
+ifconfig veth4 up
+ifconfig veth5 up
+ifconfig veth6 up
+ifconfig veth7 up
 
 ip link set veth1 netns blue
 ip link set veth3 netns red
 
 ovs-vsctl add-br br0
+ovs-vsctl add-br br1
+ovs-vsctl add-br br2
 
-ovs-vsctl add-port br0 veth0
-ovs-vsctl add-port br0 veth2
+ovs-vsctl add-port br1 veth0 # vlan 100
+ovs-vsctl add-port br2 veth2 # vlan 200
+
+ovs-vsctl add-port br1 veth4
+ovs-vsctl add-port br0 veth5
+
+ovs-vsctl add-port br2 veth6
+ovs-vsctl add-port br0 veth7
 
 ip netns exec blue ifconfig veth1 1.0.0.1 netmask 255.255.255.0
 ip netns exec red ifconfig veth3 1.0.0.2 netmask 255.255.255.0
 
 ip netns exec blue ifconfig veth1 up
-ip netns exec blue ifconfig veth3 up
+ip netns exec red ifconfig veth3 up
 
 # add the default flow entry
 ovs-ofctl add-flow br0 "actions=CONTROLLER"
